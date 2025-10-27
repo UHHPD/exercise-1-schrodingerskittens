0ed203cbf40c93f3d764296e1e894eda6307cda3
Atanu <stark@StarkLegion.localdomain>
2025-10-27 09:56:54 +0100

    minor changes

--- summieren.cc
+++ summieren.cc
@@ -1,15 +1,23 @@
 #include<iostream>
 #include<fstream>
+using namespace std;
+
 
 int main() {
     std::ifstream fin("daten.txt");
     std::ofstream fout("datensumme.txt");
     int a, b;
+    fin>>a>>b;
+    std::cout<<"First two numbers :";
+    std::cout <<a<<" and "<<b<<std::endl;
+    
+    fin.seekg(0, ios::beg);
     while(fin >> a >> b)
     {
         int sum = a + b;
         std::cout << "(" << a << " , " << b << " ) =  " << sum << std::endl;
         fout << sum << std::endl;
+        
     }
 
     fin.close();
